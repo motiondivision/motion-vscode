c7bfb31d025d20db9871a89a4064f4e11134fed7
Matt Perry <mattgperry@gmail.com>
2025-11-12 16:56:16 +0100

    Latest

--- publish-ovsx.js
+++ publish-ovsx.js
@@ -1,6 +1,24 @@
 #!/usr/bin/env node
 
 const { execSync } = require('child_process');
+const { readFileSync } = require('fs');
+const { join } = require('path');
+
+// Load .env file if it exists
+try {
+  const envFile = readFileSync(join(__dirname, '.env'), 'utf8');
+  envFile.split('\n').forEach(line => {
+    const [key, ...valueParts] = line.split('=');
+    if (key && valueParts.length > 0) {
+      const value = valueParts.join('=').trim();
+      if (!process.env[key.trim()]) {
+        process.env[key.trim()] = value;
+      }
+    }
+  });
+} catch (error) {
+  // .env file doesn't exist or can't be read, that's okay
+}
 
 const token = process.env.OPEN_VSX_TOKEN;
 
